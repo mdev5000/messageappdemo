28f23af858dab55436ab20a0dcbd4472f48e1ae0
Matt Harrison <mdev5000@fea.st>
2021-06-15 03:53:24 -0400

    Refactored db setup for data package.
    Refactored data package to use the dbpool package for setting up the test database.

--- data/testdb_test.go
+++ data/testdb_test.go
@@ -1,97 +1,15 @@
 package data
 
 import (
-	"fmt"
 	"github.com/mdev5000/qlik_message/postgres"
-	"github.com/ory/dockertest/v3"
+	"github.com/mdev5000/qlik_message/testutil/dbpool"
 	"log"
 	"os"
 	"testing"
 )
 
-const testDbUser = "pguser"
-const testDbPassword = "secret"
-const testDbName = "messagesdb"
-
-// Do not reference this directly! Use acquireDb() instead.
-var sharedDbInstance *postgres.DB
-
-// The PostgreSQL database is loaded via Docker. This function starts up a container instance of the database and
-// ensures the database can be reached.
-func setupDbPool() (*dockertest.Pool, *dockertest.Resource, error) {
-	// Uses a sensible default on windows (tcp/http) and linux/osx (socket).
-	pool, err := dockertest.NewPool("")
-	if err != nil {
-		return nil, nil, fmt.Errorf("could not connect to docker: \n%w", err)
-	}
-
-	// Pulls an image, creates a container based on it and runs it.
-	resource, err := pool.Run("postgres", "13.3", []string{
-		"POSTGRES_USER=" + testDbUser,
-		"POSTGRES_PASSWORD=" + testDbPassword,
-		"POSTGRES_DB=" + testDbName,
-	})
-	if err != nil {
-		return pool, nil, fmt.Errorf("could not start resource: \n%w", err)
-	}
-
-	// Exponential backoff-retry, because the application in the container might not be ready to accept connections yet.
-	if err := pool.Retry(func() error {
-		var err error
-		sharedDbInstance, err = postgres.OpenTest(testDbName, testDbUser, testDbPassword, resource.GetPort("5432/tcp"))
-		if err != nil {
-			return err
-		}
-		return sharedDbInstance.Ping()
-	}); err != nil {
-		return pool, resource, fmt.Errorf("could not connect to sharedDbInstance via docker: 'n%w", err)
-	}
-
-	return pool, resource, nil
-}
-
-func closeDbPool(pool *dockertest.Pool, resource *dockertest.Resource, errIsFatal bool) {
-	if err := pool.Purge(resource); err != nil {
-		if errIsFatal {
-			log.Fatalf("Could not purge resource: \n%s", err)
-		} else {
-			log.Printf("Could not purge resource: \n%s", err)
-		}
-	}
-}
-
-func setupDbSchema() error {
-	_, err := sharedDbInstance.Exec(Schema)
-	return err
-}
-
-func TestMain(m *testing.M) {
-	// Don't run any database tests.
-	if os.Getenv("NODB") == "1" {
-		os.Exit(0)
-	}
-
-	// Setup a running PostgreSQL instance.
-	pool, resource, err := setupDbPool()
-	if err != nil {
-		if pool != nil {
-			closeDbPool(pool, resource, false)
-		}
-		log.Fatalf("Failed to start pool:\n%s", err)
-	}
-
-	// Apply the required database schema.
-	if err := setupDbSchema(); err != nil {
-		log.Fatalf("Failed to create schema: \n%s", err)
-	}
-
-	code := m.Run()
-
-	// You can't defer this because os.Exit doesn't care for defer
-	closeDbPool(pool, resource, true)
-
-	os.Exit(code)
-}
+var runDbTests bool
+var pool *dbpool.DbPool
 
 // Acquire a database instance. You must call close you are finished with the database.  This functions currently
 // does 1 thing, but can potentially do 2 at some point.
@@ -110,12 +28,28 @@ func TestMain(m *testing.M) {
 // // do db stuff...
 //
 func acquireDb() (*postgres.DB, func()) {
-	return sharedDbInstance, func() {
-		purgeDatabase(sharedDbInstance)
-	}
+	return pool.AcquireDb()
 }
 
-// Removes all data from the database.
-func purgeDatabase(db *postgres.DB) {
-	db.MustExec("delete from messages")
+func TestMain(m *testing.M) {
+	// Do not run any db tests when NODB environment variable is set to 1
+	if os.Getenv("NODB") == "1" {
+		return
+	}
+
+	runDbTests = true
+	pool = dbpool.NewDbPool()
+	pool.SetupSchema = SetupSchema
+	pool.PurgeDb = PurgeDb
+
+	if err := pool.Setup(); err != nil {
+		pool.Close(false)
+		log.Fatalf("Failed to start pool:\n%s", err)
+	}
+
+	code := m.Run()
+
+	// You can't defer this because os.Exit doesn't care for defer
+	pool.Close(true)
+	os.Exit(code)
 }
--- testing/integration/messages/dbsetup_test.go
+++ testing/integration/messages/dbsetup_test.go
@@ -12,6 +12,22 @@ import (
 var runDbTests bool
 var pool *dbpool.DbPool
 
+// Acquire a database instance. You must call close you are finished with the database.  This functions currently
+// does 1 thing, but can potentially do 2 at some point.
+//
+// The first is ensure the database is in a clean state prior to running a test. This means existing database is purged
+// from the database.
+//
+// The second is thing is guarding access to database resources. Currently the database runner only has a single
+// database instance, since there is limited testing required. However, at some point it may be required to run multiple
+// database instances to improve test performance (and run the db tests in parallel). This function would then act as a
+// pool manager, serving database instances as required to test functions.
+//
+// Ex.
+// db, closeDb := acquireDb(t)
+// defer closeDb()
+// // do db stuff...
+//
 func acquireDb(t *testing.T) (*postgres.DB, func()) {
 	if !runDbTests {
 		t.SkipNow()
@@ -20,7 +36,7 @@ func acquireDb(t *testing.T) (*postgres.DB, func()) {
 }
 
 func TestMain(m *testing.M) {
-	// Do not run db tests is NODB environment variable set to 1
+	// Do not run any db tests when NODB environment variable is set to 1
 	if os.Getenv("NODB") == "1" {
 		runDbTests = false
 		os.Exit(m.Run())
